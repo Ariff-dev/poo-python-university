9bafd352df09cffb7d2ef58ae09fba7922928588
Ariff-dev <ariff.dev24@gmail.com>
2024-03-05 21:23:34 -0600

    Changes

--- Simulador_de_banco_1/implement_3.py
+++ Simulador_de_banco_1/implement_3.py
@@ -58,7 +58,7 @@ def show():
 
     #*Muestra la informacion de la cuenta_1
     print('Cuenta 0')
-    print('       nombre', nombre_cuenta_0)
+    print('       nombre:', nombre_cuenta_0)
     print('       saldo:', saldo_cuenta_0)
     print('       contraseña:', contrasenia_cuenta_0)
     print()
@@ -96,7 +96,7 @@ def getsaldo(numero_de_cuenta, contrasenia):
       
     #*Si el //parametro// = contrasenia es igual a la contrasenia de la cuenta_1
       #?Retorna el saldo de la cuenta_1
-      return saldo_cuenta_0
+      return f"Tu saldo es: {saldo_cuenta_0}"
   
   #* Si el numero de cuenta == 0
   if numero_de_cuenta == 1:
@@ -110,7 +110,7 @@ def getsaldo(numero_de_cuenta, contrasenia):
       
       #*Si el //parametro// = contrasenia es igual a la contrasenia de la cuenta_2
       #?Retorna el saldo de la cuenta_2
-      return saldo_cuenta_1
+      return f"Tu saldo es: {saldo_cuenta_1}"
   
 
 #  --- Recortamos las funciones adicionales deposito() y retiro() ---
@@ -127,10 +127,11 @@ def deposito (numero_de_cuenta, deposito, contrasenia):
       if( contrasenia != contrasenia_cuenta_0):
          #! Mostrar error
          return "Contrasenia Incorrecta"
-      
+
     #*Crear el nuevo saldo usando el saldo de la cuenta_1 + el deposito 
       nuevo_saldo = saldo_cuenta_0 + deposito
-      return f"Tu saldo actual es: ${nuevo_saldo}"
+      saldo_cuenta_0 = nuevo_saldo
+      return f"Tu saldo actual es: ${saldo_cuenta_0}"
    
 
      #* Si numero de cuenta == 1 entonces:
@@ -172,7 +173,8 @@ def retiro(numero_de_cuenta,monto_de_retiro, contrasenia):
       if(nuevo_saldo < 0):
         #! Mostrar error
          return "No tienes suficientes fondos para realizar esta accion"
-      return f"Tu saldo es: {nuevo_saldo}"
+      saldo_cuenta_0 = nuevo_saldo
+      return f"Tu saldo es: {saldo_cuenta_0}"
    
     #* Si numero de cuenta == 1 entonces:
    if( numero_de_cuenta == 1): 
@@ -187,7 +189,8 @@ def retiro(numero_de_cuenta,monto_de_retiro, contrasenia):
       if(nuevo_saldo < 0):
         #! Mostrar error
          return "No tienes suficientes fondos para realizar esta accion"
-      return f"Tu saldo es: {nuevo_saldo}"
+      saldo_cuenta_1 = nuevo_saldo
+      return f"Tu saldo es: {saldo_cuenta_1}"
       
 #  --- Tambien el código principal que las llama ---
 
@@ -197,7 +200,7 @@ while True:
   print('Presiona s para mostrar cuenta(s)') #f:show()
   print('Presiona d para hacer un deposito') #f:deposito()
   print('Presiona w para hacer un retiro') #f: retiro()
-  print('Presiona a para hacer un retiro') #f: getsaldo()
+  print('Presiona a para hacer obtener saldo') #f: getsaldo()
   print('Presiona q para salir')
   print()
 
@@ -211,15 +214,15 @@ while True:
         global nombre_cuenta_0, saldo_cuenta_0, contrasenia_cuenta_0
         global nombre_cuenta_1, saldo_cuenta_1, contrasenia_cuenta_1
         
-        nCuentas = nCuentas + 1
         if( nCuentas == 0): 
+           nCuentas = nCuentas + 1
            numero_de_cuenta = 0
            nombre = input("Escribe el nombre de la cuenta: ")
            saldo = 0
            contrasenia = input("Escribe la contraseña para la cuenta: ")
            return nueva_cuenta( numero_de_cuenta, nombre, saldo, contrasenia)
         if( nCuentas == 1):
-           numero_de_cuenta: 1
+           numero_de_cuenta = 1
            nombre = input("Escribe el nombre de la cuenta: ")
            saldo = 0
            contrasenia = input("Escribe la contraseña para la cuenta: ")
@@ -231,16 +234,17 @@ while True:
         show();
      elif action == "d":
 
-        numero_de_cuenta = input("¿En que cuenta quieres hacer el deposito?")
+        numero_de_cuenta = int(input("¿En que cuenta quieres hacer el deposito?"))
 
         if( numero_de_cuenta == 0):
            input_deposito = int(input("¿Cúanto quieres depositar?"))
            contrasenia = input("Escribe la contraseña de la cuenta: ")
 
            if( contrasenia != contrasenia_cuenta_0):
-              return "Contraseña Incorrecta"
+              print(contrasenia_cuenta_0)
+              return f"Contraseña Incorrecta: {contrasenia_cuenta_0}"
            
-           deposito(numero_de_cuenta, input_deposito, contrasenia)
+           return deposito(numero_de_cuenta, input_deposito, contrasenia)
 
         if( numero_de_cuenta == 1):
            
@@ -250,13 +254,13 @@ while True:
            if( contrasenia != contrasenia_cuenta_1):
               return "Contraseña Incorrecta"
            
-           deposito(numero_de_cuenta, input_deposito, contrasenia)
+           return deposito(numero_de_cuenta, input_deposito, contrasenia)
         else:
            return "Selecciona una cuenta existente"
           
 
      elif action == "w":
-            numero_de_cuenta = input("¿En que cuenta quieres hacer el retiro?")
+            numero_de_cuenta = int(input("¿En que cuenta quieres hacer el retiro?"))
 
             if( numero_de_cuenta == 0):
                 input_retirar = int(input("¿Cúanto quieres retirar?"))
@@ -264,7 +268,7 @@ while True:
                 if( contrasenia != contrasenia_cuenta_0):
                     return "Contraseña Incorrecta"
 
-                retiro(numero_de_cuenta, input_retirar, contrasenia)
+                return retiro(numero_de_cuenta, input_retirar, contrasenia)
 
             if( numero_de_cuenta == 1):
                 input_retirar = int(input("¿Cúanto quieres retirar?"))
@@ -272,24 +276,26 @@ while True:
                 if( contrasenia != contrasenia_cuenta_1):
                     return "Contraseña Incorrecta"
                 
-                retiro(numero_de_cuenta, input_retirar, contrasenia)
+                return retiro(numero_de_cuenta, input_retirar, contrasenia)
      elif action == "a": 
-            numero_de_cuenta = input("¿En que cuenta quieres mostrar el saldo?")
+            numero_de_cuenta = int(input("¿En que cuenta quieres mostrar el saldo?"))
 
             if( numero_de_cuenta == 0):
                contrasenia = input("Escribe la contraseña de la cuenta: ")
                if( contrasenia != contrasenia_cuenta_0):
                     return "Contraseña Incorrecta"
-               getsaldo(numero_de_cuenta, contrasenia)
+               return getsaldo(numero_de_cuenta, contrasenia)
+
             
             if( numero_de_cuenta == 1):
                contrasenia = input("Escribe la contraseña de la cuenta: ")
                if( contrasenia != contrasenia_cuenta_1):
                     return "Contraseña Incorrecta"
-               getsaldo(numero_de_cuenta, contrasenia)
+               return getsaldo(numero_de_cuenta, contrasenia)
      elif action == "q":
         return print('Bye')
+        
      
 
   print(switchAction(action))
-  print(action)
+
